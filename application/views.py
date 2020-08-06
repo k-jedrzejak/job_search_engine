4b860163c03ea016e035503788770f6a75520115
koralina <kjedrzejak91@gmail.com>
2020-08-06 23:37:01 +0200

    creating HTML

--- application/views.py
+++ application/views.py
@@ -1,9 +1,15 @@
 from application import app
 from .functions import getData, saveData, readData
+from flask import request, render_template
 from wtforms import Form, FormField, validators, TextField
 
 class Search(Form):
-    technology = TextField('Technology', validators= validators)
+    technology = TextField('Technology', validators = [validators.required()])
     location = TextField('Location')
+
+@app.route('/')
+def index():
+    form = Search(request.form)
+    return render_template('index.html', form = form)
 
 
